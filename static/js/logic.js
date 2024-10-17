dcdabd41809403a5e42eca2a549a51292096decb
robb-mm <rob_mm@msn.com>
2024-10-17 18:13:53 -0400

    state name added to popup

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,12 +10,49 @@ var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Getting our GeoJSON data
 d3.json("DataSets/model_county.geojson").then(function(data) {
-        L.geoJson(data, {
-            onEachFeature: function(feature, layer) {
-                let tempchg = feature.properties.tempchg ? feature.properties.tempchg.toFixed(2) : null;
-                // console.log(feature.properties.tempchg);
-                layer.bindPopup(`<h3>${feature.properties.NAME}</h3><hr><p>Temperature Change: ${tempchg}F<br> (years: 1895 to 2019)</p>`);
+    fetch('DataSets/model_state.csv').then(response => response.text()).then(csv => {
+        Papa.parse(csv, {
+            complete: function(results) {
+                let csvdata = results.data;
+                var st_list={};
+                // console.log(csvdata[28][8]);
+                for (let i=0; i<csvdata.length; i++)
+                    st_list[csvdata[i][0]] = csvdata[i][8];
+                // console.log(st_list["01"]);
+    
+                L.geoJson(data, {
+                    onEachFeature: function(feature, layer) {
+                        let tempchg = feature.properties.tempchg ? feature.properties.tempchg.toFixed(2) : null;
+                        // let state = get_state(st_list, feature.properties.STATEFP);
+                        let state = st_list[feature.properties.STATEFP]; // ? st_list[feature.properties.STATEFP][8] : null;
+                        // console.log(st_list[feature.properties.STATEFP]);
+                        layer.bindPopup(`<h3>${feature.properties.NAME}, ${state}</h3><hr><p>Temperature Change: ${tempchg}F<br> (year 1895 to 2019)</p>`);
+                    }
+                }).addTo(myMap);
             }
-    }).addTo(myMap);
-
+        });
+    });
 });
+
+function build_stlist(data) {
+    // let st_list = {};
+
+    for (let i=0; i<data.length; i++) {
+        st_list[data[i][0]] = data[i][8];
+    }
+    
+    // return st_list;
+}
+
+function get_state(st_list, statefp) {
+    for (let i=0; i<st_list.length; i++) {
+        // console.log(st_list);
+        if (st_list[i][0].toString() == statefp.toString()) {
+            // console.log(st_list[i][8]);
+            return st_list[i][8];
+        }
+    }
+
+    return null;
+    // console.log(state);
+}
