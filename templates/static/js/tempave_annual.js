c4167bba929dcb0acdf22be7ba802e906cd4e344
robb-mm <rob_mm@msn.com>
2024-10-22 15:57:05 -0400

    county linechart added

--- templates/static/js/tempave_annual.js
+++ templates/static/js/tempave_annual.js
@@ -1,4 +1,4 @@
-let st_list={};
+let st_list={}, cty_list={};
 // function init() {
     fetch('datasets/climdiv_state_year.csv').then(response => response.text()).then(csv => {
         Papa.parse(csv, {
@@ -27,6 +27,30 @@ let st_list={};
                 // Populate the droplist
                 for (var key in state_fips)
                     id_droplist.append("option").text(key);
+            }
+        });
+    });
+
+    fetch('datasets/climdiv_county_year.csv').then(response => response.text()).then(csv => {
+        Papa.parse(csv, {
+            complete: function(results) {
+                let csvdata = results.data;
+                let year_list=[], temp_list=[];
+                // console.log(csvdata);
+                for (let i=0; i<csvdata.length; i++) {
+                    if (year_list[0]) {
+                        year_list.push(csvdata[i][1]);
+                        temp_list.push(csvdata[i][2]);
+                    } else {
+                        year_list[0] = csvdata[i][1];
+                        temp_list[0] = csvdata[i][2];
+                    }
+                    
+                    if ((i+1 == csvdata.length) || (csvdata[i][0] != csvdata[i+1][0])) {
+                        cty_list[csvdata[i][0]] = {"year": year_list, "temp": temp_list};
+                        year_list=[], temp_list=[];
+                    }
+                }
             }
         });
     });
@@ -75,7 +99,26 @@ function buildLineChart(state_name) {
 function buildCountyLineChart(county_name) {
     let id_droplist = document.getElementById("selDataset");
     let state_name = id_droplist.options[id_droplist.selectedIndex].text;
-    console.log(county_fips[state_name]);
+    // console.log(cty_list[county_fips[state_name][county_name]].temp);
+    // console.log(county_fips[state_name][county_name]);
+
+    // Create the trace for the line chart
+    var trace = {
+        x: cty_list[county_fips[state_name][county_name]].year,
+        y: cty_list[county_fips[state_name][county_name]].temp,
+        mode: 'lines+markers',
+        type: 'scatter',
+        line: {color: 'blue'}
+    };
+    // console.log(trace.y);
+    var layout = {
+        title: 'Average Temperature Per Year',
+        xaxis: { title: 'Year' },
+        yaxis: { title: 'Average Temperature (Fahrenheit)' }
+    };
+
+    // Render the plot
+    Plotly.newPlot('chart', [trace], layout);
 }
 
 // Function for event listener
@@ -87,9 +130,9 @@ function optionChanged(state_name) {
 }
 
 function countyoptionChanged(county_name) {
-    buildCountyLineChart(event.currentTarget.value);
-
     console.log(event.currentTarget.value);
+    
+    buildCountyLineChart(event.currentTarget.value);
 }
 
 // Initialize the dashboard
